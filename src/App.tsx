6ee7a032aabed70624e3d40486b3629bd2a8925c
omer <oemer.peynirci@ontavio.de>
2022-11-28 22:19:43 +0100

    ts errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import styles from './App.module.css';
-
+import {FormEvent, useState} from 'react';
+import text from './data';
 function App() {
+    const [count,setCount]=useState(1);
+    const [resultText, setResultText]=useState<string[]|null>(null);
+    const updateCount=(num:number)=>{
+        if(num<1){
+            setCount(1);
+        } else
+        if (num>8) { setCount(8)}else{
+            setCount(num);
+        }
+
+
+    }
+    const handleSubmit=(e:FormEvent)=>{
+        e.preventDefault();
+        setResultText(text.slice(0,count))
+
+    }
 
   return (
     <div className={styles.App}>
       <h1 className={styles.title}>tired of boring lorem ipsum?</h1>
-        <form>
+        <form onSubmit={(e)=>handleSubmit(e)}>
             <div className={styles.formRow}>
-                <label htmlFor="paragraphCount">Paragraphs:</label> <input id="paragraphCount" type="number" className={styles.inputCount}/> <button className={styles.btnGenerate}>GENERATE</button>
+                <label htmlFor="paragraphCount">Paragraphs:</label>
+                <input id="paragraphCount" type="number" className={styles.inputCount} value={count} onChange={(e)=>updateCount(Number(e.target.value))} required/>
+                <button className={styles.btnGenerate}>GENERATE</button>
             </div>
         </form>
 
         <div className={styles.allParagraphs}>
-            <p className={styles.paragraph}>Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.</p>
+            {resultText?.map((t,key)=> <p key={key} className={styles.paragraph}>{t}</p>)}
+
         </div>
     </div>
   )
